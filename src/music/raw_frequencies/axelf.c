ea81aff76aa335414c6e85111071ca005008da8e
Alvin Rhaman <50039002+alvinneocom@users.noreply.github.com>
2020-01-06 00:04:08 -0600

    (new) axel f

---
+++ src/music/raw_frequencies/axelf.c
@@ -0,0 +1,27 @@
+#include "../beep_structure.c"
+
+struct beep axel[] = {
+    {.freq = 659, .len = 460},
+    {.freq = 784, .len = 340},
+    {.freq = 659, .len = 230},
+    {.freq = 659, .len = 110},
+    {.freq = 880, .len = 230},
+    {.freq = 659, .len = 230},
+    {.freq = 587, .len = 230},
+    {.freq = 659, .len = 460},
+    {.freq = 988, .len = 340},
+    {.freq = 659, .len = 230},
+    {.freq = 659, .len = 110},
+    {.freq = 1047, .len = 230},
+    {.freq = 988, .len = 230},
+    {.freq = 784, .len = 230},
+    {.freq = 659, .len = 230},
+    {.freq = 988, .len = 230},
+    {.freq = 1318, .len = 230},
+    {.freq = 659, .len = 110},
+    {.freq = 587, .len = 230},
+    {.freq = 587, .len = 110},
+    {.freq = 494, .len = 230},
+    {.freq = 740, .len = 230},
+    {.freq = 659, .len = 460}
+};
